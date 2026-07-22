07d6ec91e99adf8f0bea7d44cb121aceef9a8718
agarwalvarun24 <agarwalvarun2403@gmail.com>
2026-07-23 01:10:50 +0530

    Add health routes and CORS configuration

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const healthRoutes = require("./routes/health.routes");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/api/health", healthRoutes);
 
 app.get("/", (req, res) => {
     res.json({
