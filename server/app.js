b5ba8ddc6b352d998808f93c6e745728939b27b6
agarwalvarun24 <agarwalvarun2403@gmail.com>
2026-07-23 00:56:36 +0530

    Setup express server

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Crime Intelligence Platform API Running"
+    });
+});
+
+module.exports = app;
