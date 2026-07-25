85afd39273fbdb873597ab77e2198f5ed43ae7c0
Yash5217 <agarwaly609@gmail.com>
2026-07-25 16:30:35 +0530

    Analytics front

--- client/src/pages/Analytics/Analytics.jsx
+++ client/src/pages/Analytics/Analytics.jsx
@@ -1,6 +1,30 @@
+import SummaryCards from "../../components/charts/SummaryCards";
+import CrimeTrendChart from "../../components/charts/CrimeTrendChart";
+import CrimeTypePieChart from "../../components/charts/CrimeTypePieChart";
+import DistrictBarChart from "../../components/charts/DistrictBarChart";
+import AIInsights from "../../components/charts/AIInsights";
+
 function Analytics() {
   return (
-    <h1>Analytics Page</h1>
+    <div className="p-6 space-y-6">
+
+      <h1 className="text-3xl font-bold text-white">
+        Crime Analytics Dashboard
+      </h1>
+
+      <SummaryCards />
+
+      <div className="grid lg:grid-cols-2 gap-6">
+        <CrimeTrendChart />
+        <CrimeTypePieChart />
+      </div>
+
+      <div className="grid lg:grid-cols-2 gap-6">
+        <DistrictBarChart />
+        <AIInsights />
+      </div>
+
+    </div>
   );
 }
 
